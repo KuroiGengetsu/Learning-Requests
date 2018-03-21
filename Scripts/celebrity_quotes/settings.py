9a9be5b687724ff35282dfb987c517bcd36cc793
KuroiGengetsu <1198148133@qq.com>
2018-03-22 06:42:17 +0800

    fix lots of problems

---
+++ Scripts/celebrity_quotes/settings.py
@@ -0,0 +1,12 @@
+"""Settings.py: All configurations are here"""
+
+HEADERS = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8;',
+        'accept-language': 'zh_CN,zh;q=0.8',
+        'user-agent': 'Chrome/61.0.3293.100 Mozilla/5.0(Windows NT 6.3) AppleWebKit/537.87(KHTML,like Gecko) Safari/537.65'
+    }
+
+DEBUG = "---DEBUG---: "
+
+UNWANTED = ["&mdash;", "&middot;", "&rdquo;", "&ldquo;", "&quot;", "&hellip;", "&amp;", "&lsquo;", "&rsquo;", "&nbsp;"]
+
